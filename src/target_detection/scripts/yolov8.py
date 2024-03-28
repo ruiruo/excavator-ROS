23906a01359937a621c65f6f97f67ae8e030bdca
lisq58 <1323614196@qq.com>
2024-03-28 02:25:41 -0700

    fix : 将关键点的数据处理全部使用tensor张量完成

--- src/target_detection/scripts/yolov8.py
+++ src/target_detection/scripts/yolov8.py
@@ -11,6 +11,7 @@ from scipy.spatial.transform import Rotation as R
 import scipy.constants as C
 import threading
 import apriltag
+import torch
 
 import rospy
 from geometry2.tf2_ros.src import tf2_ros
@@ -28,9 +29,10 @@ class TargetDetection:
         self.half = rospy.get_param('~half', 'False')
         self.show = rospy.get_param('~show', 'False')# 显示实时画面
         self.conf = rospy.get_param('~conf', '0.5')
-        self.point_search_range = rospy.get_param('~point_search_range', '4')
+        self.point_search_range = int(rospy.get_param('~point_search_range', '4'))
         self.model = YOLO(weight_path, task='pose')
         self.debug = rospy.get_param('~debug', 'False')
+        self.target_box = rospy.get_param('~target_box', 'False')
 
         # 选择yolo运行设备
         if (rospy.get_param('~cpu', 'False')):
@@ -38,7 +40,6 @@ class TargetDetection:
         else:
             self.device = '0'
 
-        self.TargetPoints = Target_Points()
         self.cam_broadcaster = tf2_ros.StaticTransformBroadcaster()
         self.color_cam_to_body_tf = TransformStamped()
         self.yaw = 0.
@@ -91,7 +92,7 @@ class TargetDetection:
 
         # 主循环
         while not rospy.is_shutdown() :
-            try:
+            # try:
             # 图像获取与预处理12ms 主要为旋转耗时8ms
                 frames = self.pipeline.wait_for_frames()
                 aligned_frames = align.process(frames)
@@ -111,9 +112,9 @@ class TargetDetection:
                     self.color_image = np.asanyarray(color_frame.get_data())
                     self.color_image = cv2.rotate(self.color_image, cv2.ROTATE_90_CLOCKWISE)
                     self.depth_image = cv2.rotate(self.depth_image, cv2.ROTATE_90_CLOCKWISE)
+                    self.depth_image = self.depth_image.astype(np.float32)
+                    self.depth_image = torch.from_numpy(self.depth_image)
                     forearm_receive.set()
-                    # YOLO推理与数据处理
-                    self.predict()
 
                 # 将校正后得到的相机外参用于关键点转换
                 if((self.calibrate_array_size >= 250) and
@@ -126,11 +127,11 @@ class TargetDetection:
 
                 # 使用相机外参对关键点转换并发布 耗时2-4ms
                 if self.conversion_buffer.can_transform("camera_color_frame", "body",  rospy.Time()):
-                    self._transform_kp()
+                    self._transform_kp(self.predict())
                     self.position_pub.publish(self.coordinate_points)
-            except Exception as e:
-                rospy.logwarn_throttle_identical(60,\
-                                                 "Unable to obtain camera image data, check if camera is working.")
+            # except Exception as e:
+            #     rospy.logwarn_throttle_identical(60,\
+            #                                      "Unable to obtain camera image data, check if camera is working.")
         self.pipeline.stop()
         cv2.destroyAllWindows()
 
@@ -147,18 +148,19 @@ class TargetDetection:
                 except Exception as e:
                     pass
 
-            # 数据整理与处理9-12ms 整理耗时约7ms
             if any(results[0].boxes.cls):
-                boxes = [
-                    [results[0].names[int(cls)]] + [float(conf)] + xywh.tolist() + keypoints_xy.tolist()
-                    for cls, conf, xywh, keypoints_xy in zip(
-                        results[0].boxes.cls,
-                        results[0].boxes.conf,
-                        results[0].boxes.xywh,
-                        results[0].keypoints.xy)]
-                self._data_process(boxes)#spend 2ms
-            else:
-                rospy.logwarn_throttle_identical(10,"No valid targets detected")
+                num = len(results[0].boxes.cls)
+                if self.target_box:
+                    boxes = torch.stack([torch.cat((cls, conf, xywh), dim=0)\
+                                    for cls, conf, xywh in zip(
+                                        results[0].boxes.cls.reshape(num, 1),
+                                        results[0].boxes.conf.reshape(num, 1),
+                                        results[0].boxes.xywh)])
+                if len(results[0].boxes.cls) > 1:
+                    keypoints = torch.stack([*results[0].keypoints.xy], dim=1).view(-1, num, 2)
+                else:
+                    keypoints = results[0].keypoints.xy
+                return self._data_process(keypoints)
 
     def forearm_thread(self, forearm_receive):
         while not rospy.is_shutdown():
@@ -185,6 +187,7 @@ class TargetDetection:
         while not rospy.is_shutdown():
         # 接收到加速度信息更新加速度数据
             while accel_receive.is_set():
+                print(self.calibrate_array_size)
                 try:
                     frames = self.pipeline.wait_for_frames()
                     accel_frame = frames.first_or_default(rs.stream.accel)
@@ -205,63 +208,45 @@ class TargetDetection:
                     break
 
     # 将关键点坐标通过(彩色相机->机体)旋转矩阵进行变换
-    def _transform_kp(self): 
-        for p in self.TargetPoints.key_points:
-            coordinate_point = Coordinate_point()
-            coordinate_point.name = p.name
-            point = np.array([[p.x], [p.y], [1]])
-            if p.distance != 0:
-                point = np.dot(p.distance * self.inv_K , point)
-                coordinate_point.x, coordinate_point.y, coordinate_point.z = -np.dot(
-                    point.T, self.R_color_to_body.as_matrix()).T
-                coordinate_point.x = -coordinate_point.x
-            #x,y,z 前 右 下
-            self.coordinate_points.coordinate_points.append( coordinate_point)
+    def _transform_kp(self, kp_tensor): 
+        transform_tensor = torch.zeros(kp_tensor.size(0), kp_tensor.size(1), 3, dtype=float)
+        for row in range(kp_tensor.size(0)):
+            for col in range(kp_tensor.size(1)):
+                point = torch.tensor([[kp_tensor[row][col][0]], [kp_tensor[row][col][1]], [1.]])
+                if kp_tensor[row][col][2] != 0:
+                    point = torch.matmul(kp_tensor[row][col][2] * torch.from_numpy(self.inv_K) , point)
+                    transform_tensor[row][col] = -torch.matmul(point.squeeze(), self.R_color_to_body_tensor).squeeze()
+                    transform_tensor[row][col][0] = -transform_tensor[row][col][0]
+            #x,y,z 前 右 下 tensor float64
+        return transform_tensor
 
-    # 根据是否校准处理关键点数据
-    def _data_process(self, boxes):
-        for box in boxes:
-            self._collect_boxes(box[:6])
-            self._collect_keypoints(box[6:])
-
-    # 处理物体框数据
-    def _collect_boxes(self, box):
-        BoxPoint = Box_Point(
-            probability=box[1],
-            distance=self._get_point_distance([int(box[2]), int(box[3])]),
-            x=box[2], y=box[3],
-            width=box[4],  height=box[5],
-            name=box[0]
-        )
-        self.TargetPoints.box_points.append(BoxPoint)
-
-    # 处理关键点数据
-    def _collect_keypoints(self, keypoints):
-        kp_name = ['A', 'B', 'A_d', 'B_d', 'BF_l', 'BF_r']
-        self.TargetPoints.key_points.extend(
-            Key_Point(
-                x=kp[0], y=kp[1],
-                distance=self._get_point_distance([int(kp[0]), int(kp[1])]),
-                name=name
-            ) for name, kp in zip(kp_name, keypoints) if self._get_point_distance([int(kp[0]), int(kp[1])]) != 0.
-        )
+    def _data_process(self, kp_tensor):
+        add_distance_tensor = torch.zeros(kp_tensor.size(0), kp_tensor.size(1), 3, dtype=float)
+        for i in range(kp_tensor.size(0)):
+            for j in range(kp_tensor.size(1)):
+                mid_pos = kp_tensor[i][j]
+                distance = self._get_point_distance(mid_pos.int())
+                add_distance_tensor[i][j] = torch.cat((mid_pos, torch.tensor([distance])))
+        return add_distance_tensor
 
     # 获取指定关键点距离，距离单位cm 原始数据单位为mm
     def _get_point_distance(self, mid_pos):
-        distance_list = np.empty((1, 0))
-        for scope in range(int(self.point_search_range)):
+        distance_list = torch.tensor([])
+        for scope in range(self.point_search_range):
             if (mid_pos[0] + scope < self.depth_image.shape[1] and 
-                mid_pos[1] + scope < self.depth_image.shape[0]):
-                distance_list = np.array([self.depth_image[int(mid_pos[1]+i)][int(mid_pos[0]+j)]
-                                        for i in range(-scope, scope+1) for j in range(-scope, scope+1)])
-            if np.sum(distance_list) != 0 :
+            mid_pos[1] + scope < self.depth_image.shape[0]):
+                distance_list = self.depth_image[
+                    int(mid_pos[1]-scope):int(mid_pos[1]+scope+1),
+                    int(mid_pos[0]-scope):int(mid_pos[0]+scope+1)
+                ].flatten()
+            if torch.sum(distance_list) != 0:
                 break
         distance_list = distance_list[distance_list != 0]
-        if len(distance_list)>=4:
-            distance_list = np.percentile(distance_list, [25, 50, 75])
+        if len(distance_list) >= 4:
+            distance_list = torch.quantile(distance_list, torch.tensor([0.25, 0.5, 0.75]))
         elif len(distance_list) == 0:
-            return 0. 
-        return np.round(np.mean(distance_list) / 10, 4)
+            return 0.
+        return torch.round(torch.mean(distance_list)* 1000) / 10000
         
     # RGBD相机外参旋转矩阵获取
     def _calibrate(self):
@@ -288,7 +273,8 @@ class TargetDetection:
         R_world_to_accel = R.from_euler('XYZ', [0, roll, 0], degrees=True)
         R_world_to_color = R_world_to_accel * R_aceel_to_color
         #缺少body与world的旋转矩阵 yaw直接参考机体 roll与pitch参考世界坐标系
-        self.R_color_to_body = R_body_to_color_yaw*R_world_to_color
+        R_color_to_body = (R_body_to_color_yaw * R_world_to_color)
+        self.R_color_to_body_tensor = torch.tensor(R_color_to_body.as_matrix(), dtype=torch.float64)
 
         self.color_cam_to_body_tf.header.frame_id = "camera_color_frame"
         self.color_cam_to_body_tf.header.stamp = rospy.Time.now()
@@ -297,10 +283,10 @@ class TargetDetection:
         self.color_cam_to_body_tf.transform.translation.x = 0.
         self.color_cam_to_body_tf.transform.translation.y = 0.
         self.color_cam_to_body_tf.transform.translation.z = 0.
-        self.color_cam_to_body_tf.transform.rotation.x = self.R_color_to_body.as_quat()[0]
-        self.color_cam_to_body_tf.transform.rotation.y = self.R_color_to_body.as_quat()[1]
-        self.color_cam_to_body_tf.transform.rotation.z = self.R_color_to_body.as_quat()[2]
-        self.color_cam_to_body_tf.transform.rotation.w = self.R_color_to_body.as_quat()[3]
+        self.color_cam_to_body_tf.transform.rotation.x = R_color_to_body.as_quat()[0]
+        self.color_cam_to_body_tf.transform.rotation.y = R_color_to_body.as_quat()[1]
+        self.color_cam_to_body_tf.transform.rotation.z = R_color_to_body.as_quat()[2]
+        self.color_cam_to_body_tf.transform.rotation.w = R_color_to_body.as_quat()[3]
         self.cam_broadcaster.sendTransform(self.color_cam_to_body_tf)
 
     # 测试指定函数段耗时辅助函数
